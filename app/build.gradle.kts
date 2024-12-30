7c56a7f60dd3428db7b1224d12d920f5620edd63
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2024-12-30 22:12:09 +0800

    将Gradle迁移到Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,107 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+android {
+    androidResources {
+        generateLocaleConfig = true
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    signingConfigs {
+        create("AceKeystore") {
+            keyAlias = System.getenv("KEY_ALIAS") ?: keystoreProperties["keyAlias"] as String
+            keyPassword =
+                System.getenv("KEY_PASSWORD") ?: keystoreProperties["keyPassword"] as String
+            storeFile =
+                if (System.getenv("STORE_FILE") != null && System.getenv("STORE_FILE") != "") file("../keystore.jks") else file(
+                    keystoreProperties["storeFile"] as String
+                )
+            storePassword =
+                System.getenv("STORE_PASSWORD") ?: keystoreProperties["storePassword"] as String
+        }
+    }
+
+    defaultConfig {
+        applicationId = "io.github.acedroidx.frp"
+        minSdk = 23
+        targetSdk = 35
+        compileSdk = 35
+        versionCode = 5
+        versionName = "1.0.4"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        signingConfig = signingConfigs.getByName("AceKeystore")
+
+        buildConfigField("String", "FrpVersion", "\"0.61.1\"")
+        buildConfigField("String", "FrpcFileName", "\"libfrpc.so\"")
+        buildConfigField("String", "ConfigFileName", "\"config.toml\"")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                // Includes the default ProGuard rules files that are packaged with
+                // the Android Gradle plugin. To learn more, go to the section about
+                // R8 configuration files.
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                // Includes a local, custom Proguard rules file
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("AceKeystore")
+        }
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("AceKeystore")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("arm64-v8a", "x86_64", "armeabi-v7a")
+            isUniversalApk = true
+        }
+    }
+    namespace = "io.github.acedroidx.frp"
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-service:2.8.7")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
