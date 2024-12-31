82ecac7efcfa50bf64c1568916d75977961c7c9e
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2024-12-31 18:20:00 +0800

    修复Shell线程没有被终止的问题

--- app/src/main/java/io/github/acedroidx/frp/ShellThread.kt
+++ app/src/main/java/io/github/acedroidx/frp/ShellThread.kt
@@ -1,6 +1,5 @@
 package io.github.acedroidx.frp
 
-import android.util.Log
 import java.io.BufferedReader
 import java.io.File
 import java.io.IOException
@@ -18,11 +17,17 @@ class ShellThread(
             val process = Runtime.getRuntime().exec(command, envp, dir)
             val inputStream = process.inputStream
             val reader = BufferedReader(InputStreamReader(inputStream))
-            var line: String?
-            while (((reader.readLine().also { line = it }) != null) && !isInterrupted) {
-//                outputBuilder.insert(0, line).append("\n")
-//                outputBuilder.append(line).append("\n")
-                line?.let { outputCallback(it) }
+            // https://8thlight.com/insights/handling-blocking-threads-in-java
+            while (!isInterrupted) {
+                if (!reader.ready()) {
+                    try {
+                        sleep(100)
+                        continue
+                    } catch (e: InterruptedException) {
+                        break
+                    }
+                }
+                reader.readLine()?.let { outputCallback(it) }
             }
             reader.close()
             process.destroy()
