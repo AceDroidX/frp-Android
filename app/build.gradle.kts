919ae1f5228cb79f807f3c064259525a51d7cf16
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2025-02-18 08:32:25 +0800

    v1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         minSdk = 23
         targetSdk = 35
         compileSdk = 35
-        versionCode = 7
-        versionName = "1.2.0"
+        versionCode = 8
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
