061d72d698a931b325268e59ec12f22bac8c80f7
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2026-05-24 10:45:33 +0000

    build: frp添加构建web资产的步骤

--- scripts/build_frp_binaries.sh
+++ scripts/build_frp_binaries.sh
@@ -2,6 +2,14 @@ NDK_ROOT=${NDK_ROOT:-/ndk}
 FRP_ROOT=${FRP_ROOT:-/frp}
 FRP_ANDROID_ROOT=${FRP_ANDROID_ROOT:-/workspaces/frp-Android}
 
+echo "Start building web assets (frps)"
+cd $FRP_ROOT/web/frps
+make build
+
+echo "Start building web assets (frpc)"
+cd $FRP_ROOT/web/frpc
+make build
+
 cd $FRP_ROOT
 
 # https://dave.engineer/blog/2025/11/cross-compiling-go-android/
@@ -9,17 +17,17 @@ echo "Start building frp binaries for Android..."
 
 echo "arm64-v8a"
 export CC=$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android36-clang
-GOOS=android GOARCH=arm64 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frpc,noweb -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/arm64-v8a/libfrpc.so ./cmd/frpc
-GOOS=android GOARCH=arm64 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frps,noweb -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/arm64-v8a/libfrps.so ./cmd/frps
+GOOS=android GOARCH=arm64 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frpc -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/arm64-v8a/libfrpc.so ./cmd/frpc
+GOOS=android GOARCH=arm64 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frps -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/arm64-v8a/libfrps.so ./cmd/frps
 
 echo "x86_64"
 export CC=$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android36-clang
-GOOS=android GOARCH=amd64 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frpc,noweb -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/x86_64/libfrpc.so ./cmd/frpc
-GOOS=android GOARCH=amd64 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frps,noweb -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/x86_64/libfrps.so ./cmd/frps
+GOOS=android GOARCH=amd64 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frpc -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/x86_64/libfrpc.so ./cmd/frpc
+GOOS=android GOARCH=amd64 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frps -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/x86_64/libfrps.so ./cmd/frps
 
 echo "armeabi-v7a"
 export CC=$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi36-clang
-GOOS=android GOARCH=arm GOARM=7 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frpc,noweb -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/armeabi-v7a/libfrpc.so ./cmd/frpc
-GOOS=android GOARCH=arm GOARM=7 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frps,noweb -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/armeabi-v7a/libfrps.so ./cmd/frps
+GOOS=android GOARCH=arm GOARM=7 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frpc -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/armeabi-v7a/libfrpc.so ./cmd/frpc
+GOOS=android GOARCH=arm GOARM=7 CGO_ENABLED=1 go build -trimpath -ldflags "-s -w -checklinkname=0" -tags frps -o $FRP_ANDROID_ROOT/app/src/main/jniLibs/armeabi-v7a/libfrps.so ./cmd/frps
 
 echo "Done!"
